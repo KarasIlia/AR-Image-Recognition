702f7ccf54dc57e38d13f47d901f20afee2b6c66
Ilia <terpariz@mail.ru>
2019-06-02 20:57:41 +0300

    AR 500rub to 1000usd

--- AR Image Recognition/ViewController.swift
+++ AR Image Recognition/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -23,19 +23,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil)!
+        
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        configuration.detectionImages = referenceImages
+        configuration.maximumNumberOfTrackedImages = 2
+        configuration.planeDetection = [.horizontal, .vertical]
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -46,30 +47,79 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+}
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
+// MARK: - ARSCNViewDelegate
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        switch anchor {
+        case let imageAnchor as ARImageAnchor:
+            nodeAdded(node, for: imageAnchor)
+        case let planeAnchor as ARPlaneAnchor:
+            nodeAdded(node, for: planeAnchor)
+        default:
+            print(#line, #function, "\(anchor) added")
+        }
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        node.removeFromParentNode()
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
+    func nodeAdded(_ node: SCNNode, for anchor: ARImageAnchor) {
         
+        let referenceImage = anchor.referenceImage
+        let size = referenceImage.physicalSize
+        let plane = SCNPlane(width: 1.02 * size.width, height: 1.02 * size.height)
+        
+        switch referenceImage.name {
+        case "500faceLeft":
+            plane.firstMaterial?.diffuse.contents = UIImage(named: "1000USDfaceLeft")
+            
+            remove(anchor)
+        case "500faceRight":
+            plane.firstMaterial?.diffuse.contents = UIImage(named: "1000USDfaceRight")
+            
+            remove(anchor)
+        case "500backLeft":
+            plane.firstMaterial?.diffuse.contents = UIImage(named: "1000USDbackLeft")
+            
+            remove(anchor)
+        case "500backRight":
+            plane.firstMaterial?.diffuse.contents = UIImage(named: "1000USDbackRight")
+            
+            remove(anchor)
+        default:
+            break
+        }
+        
+        let planeNode = SCNNode(geometry: plane)
+        
+        planeNode.eulerAngles.x = -.pi / 2
+        
+        node.addChildNode(planeNode)
+    }
+    
+    func remove(_ anchor: ARImageAnchor) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+            self.sceneView.session.remove(anchor: anchor)
+        })
+    }
+    
+    func nodeAdded(_ node: SCNNode, for anchor: ARPlaneAnchor) {
+        let extent = anchor.extent
+        let width = CGFloat(extent.x)
+        let height = CGFloat(extent.z)
+        let plane = SCNPlane(width: width, height: height)
+        
+        plane.firstMaterial?.diffuse.contents = UIColor.blue
+        
+        let planeNode = SCNNode(geometry: plane)
+        
+        planeNode.eulerAngles.x = -.pi / 2
+        planeNode.opacity = 0
+        
+        node.addChildNode(planeNode)
     }
 }
+
